7d274e4120886ca25a715d5cf8b8ca56bd28d738
danialpezeshpoor <dpezeshpoor@yahoo.com>
2023-12-16 19:58:36 +0330

    arrays and hashing: contains duplicate

---
+++ arrayAndHashing/Contains Duplicate/main.go
@@ -0,0 +1,23 @@
+package main
+
+func containsDuplicate(nums []int) bool {
+	// Check for empty array
+	if len(nums) < 1 {
+		return true
+	}
+
+	// Keep track of existence of elements that are visited before
+	exists := make(map[int]struct{})
+
+	for _, n := range nums {
+		// Check if element is already in existence array
+		if _, ok := exists[n]; ok {
+			return true
+		} else {
+			exists[n] = struct{}{}
+		}
+	}
+
+	// No duplicated elements
+	return false
+}
