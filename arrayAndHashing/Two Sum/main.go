9834140d2352c62e67d7fe8b4f363d26fb2ce163
danialpezeshpoor <dpezeshpoor@yahoo.com>
2023-12-21 08:37:07 +0330

    arrays and hash-maps: two sum

---
+++ arrayAndHashing/Two Sum/main.go
@@ -0,0 +1,18 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	var result [2]int
+	tracker := make(map[int]int)
+
+	for idx1, num := range nums {
+		complementary := target - num
+		if idx2, isThere := tracker[complementary]; isThere {
+			result[0], result[1] = idx2, idx1
+			break
+		}
+
+		tracker[num] = idx1
+	}
+
+	return result[:]
+}
