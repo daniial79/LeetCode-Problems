643f98390200fad87e5ffe46aa06e7906b9c1d18
danialpezeshpoor <dpezeshpoor@yahoo.com>
2024-02-29 13:25:16 +0330

    arrays and hashing - group anagrams

---
+++ arraysAndHashing/groupAnagrams.go
@@ -0,0 +1,22 @@
+package main
+
+func groupAnagrams(strs []string) [][]string {
+	anagramBuckets := make(map[[26]int][]string)
+
+	for _, str := range strs {
+		var count [26]int
+		for _, char := range str {
+			count[char-'a']++
+		}
+		anagramBuckets[count] = append(anagramBuckets[count], str)
+	}
+
+	idx := 0
+	result := make([][]string, len(anagramBuckets))
+	for _, bucket := range anagramBuckets {
+		result[idx] = bucket
+		idx++
+	}
+
+	return result
+}
